7b224712f1d2b53e4deb101e6dec8fac01c403bb
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2024-08-20 17:50:31 +0200

    task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_20_151314) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_20_154856) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "status"
+    t.date "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
